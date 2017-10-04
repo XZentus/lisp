4574e74455c9c494f58e1fe2144ca5400489ce0e
XZentus <z_xmn@mail.ru>
2017-10-04 13:13:54 +0300

    Update http-nalog.lisp

--- http-nalog.lisp
+++ http-nalog.lisp
@@ -1,19 +1,30 @@
+(ql:quickload :cl-ppcre)
+(ql:quickload :dexador)
+
 (defparameter token-url "https://service.nalog.ru/static/captcha.html")
 (defparameter tmp-file "d:/src/lisp/tmp.gif")
-(defparameter *cookie-jar* (cl-cookie:make-cookie-jar))
+(defvar *cookie-jar* (cl-cookie:make-cookie-jar))
 
-(dex:head "https://service.nalog.ru/inn.do" :cookie-jar *cookie-jar*)
+(defvar *main-url* "https://service.nalog.ru/inn.do")
 
-(defparameter current-token (dex:get token-url :cookie-jar *cookie-jar*))
+(defvar current-token)
+
+(multiple-value-bind (_ token)
+    (ppcre:scan-to-strings "name=\"captchaToken\" value=\"([0-9A-Z]+)\"" 
+			   (dex:get *main-url* :cookie-jar *cookie-jar*))
+  (setf current-token (aref token 0)))
+
 (defparameter captcha-picture (dex:get
-			       (concatenate 'string token-url "?a=" current-token)
+			       (concatenate 'string token-url
+					    "?a=" current-token)
 			       :cookie-jar *cookie-jar*))
 
 (with-open-file (out tmp-file
 		     :direction :output
 		     :if-exists :supersede
+		     :if-does-not-exist :create
 		     :element-type 'unsigned-byte)
-	   (write-sequence captcha-picture out))
+  (write-sequence captcha-picture out))
 
 (asdf:run-shell-command tmp-file)
 (defparameter captcha-result (read-line))
@@ -21,9 +32,9 @@
 (defparameter ans (dex:post "https://service.nalog.ru/inn-proc.do"
 			    :cookie-jar *cookie-jar*
 			    :content '(("c" . "innMy")
-				       ("fam" . "test")
-				       ("nam" . "test")
-				       ("otch" . "test")
+				       ("fam" . "тест")
+				       ("nam" . "тест")
+				       ("otch" . "тест")
 				       ("bdate" . "11.11.1111")
 				       ("bplace" . "")
 				       ("doctype" . "21")
