12e1489f6239fb7530197f044cb55458424fb33d
XZentus <z_xmn@mail.ru>
2018-10-11 15:11:05 +0300

    Update util.lisp

--- util.lisp
+++ util.lisp
@@ -1,13 +1,15 @@
-(defmacro deflazy/memo (name args &body body)
-  (let ((h (gensym "HASH"))
-        (result (gensym "RESULT"))
-        (declarations ()))
-    (when (and (consp body)
-               (consp (car body))
-               (equal 'declare (caar body)))
-      (setf declarations (car body)
-            body (cdr body)))
-    (symbol-macrolet ((hash `(gethash (list ,@args) ,h)))
+(symbol-macrolet ((declares (when (and (consp body)
+                                       (consp (car body))
+                                       (equal 'declare (caar body)))
+                              (setf declarations (car body)
+                                    body (cdr body))))
+                  (hash `(gethash (list ,@args) ,h)))
+  
+  (defmacro deflazy/memo (name args &body body)
+    (let ((h (gensym "HASH"))
+          (result (gensym "RESULT"))
+          (declarations ()))
+      declares
       `(let ((,h (make-hash-table :test #'equal)))
          (defun ,name ,args
            ,declarations
@@ -18,17 +20,12 @@
                        (lambda ()
                          (let ((,result (progn ,@body)))
                            (setf ,hash (lambda () ,result))
-                           ,result))))))))))
+                           ,result)))))))))
 
-(defmacro defmemo (name args &body body)
-  (let ((h (gensym "HASH"))
-        (declarations ()))
-    (when (and (consp body)
-               (consp (car body))
-               (equal 'declare (caar body)))
-      (setf declarations (car body)
-            body (cdr body)))
-    (symbol-macrolet ((hash `(gethash (list ,@args) ,h)))
+  (defmacro defmemo (name args &body body)
+    (let ((h (gensym "HASH"))
+          (declarations ()))
+      declares
       `(let ((,h (make-hash-table :test #'equal)))
          (defun ,name ,args
            ,declarations
@@ -37,12 +34,13 @@
                  val
                  (setf ,hash (progn ,@body)))))))))
 
+
 (defmacro deflazy (name args &body body)
   `(defun ,name ,args (lambda () ,@body)))
 
 (deflazy/memo fib-lazymemo (n)
   (declare (fixnum n))
-;  (format t "Evaluating: (fib-lazymemo ~A) ...~%" n)
+                                        ;  (format t "Evaluating: (fib-lazymemo ~A) ...~%" n)
   (if (<= n 2)
       1
       (+ (the integer (funcall (fib-lazymemo (1- n))))
@@ -50,21 +48,21 @@
 
 (defun fib-slow (n)
   (declare (fixnum n))
-;  (format t "Evaluating: (fib-slow ~A) ...~%" n)
+                                        ;  (format t "Evaluating: (fib-slow ~A) ...~%" n)
   (if (<= n 2)
       1
       (+ (fib-slow (1- n)) (fib-slow (- n 2)))))
 
 (defmemo fib-memo (n)
   (declare (fixnum n))
-;  (format t "Evaluating: (fib-memo ~A) ...~%" n)
+                                        ;  (format t "Evaluating: (fib-memo ~A) ...~%" n)
   (if (<= n 2)
       1
       (+ (fib-memo (1- n)) (fib-memo (- n 2)))))
 
 (deflazy fib-lazy (n)
   (declare (fixnum n))
-;  (format t "Evaluating: (fib-memo ~A) ...~%" n)
+                                        ;  (format t "Evaluating: (fib-memo ~A) ...~%" n)
   (if (<= n 2)
       1
       (+ (funcall (fib-lazy (1- n))) (funcall (fib-lazy (- n 2))))))
