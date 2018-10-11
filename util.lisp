8e655c8a830d7d024331874f4f012351a8cbdf7e
XZentus <z_xmn@mail.ru>
2018-10-11 12:51:47 +0300

    Update util.lisp

--- util.lisp
+++ util.lisp
@@ -1,6 +1,12 @@
-(defmacro deflazy/memo-declare (name args declarations &body body)
+(defmacro deflazy/memo (name args &body body)
   (let ((h (gensym "HASH"))
-        (result (gensym "RESULT")))
+        (result (gensym "RESULT"))
+        (declarations ()))
+    (when (and (listp body)
+               (listp (car body))
+               (equal 'declare (caar body)))
+      (setf declarations (car body)
+            body (cdr body)))
     `(let ((,h (make-hash-table :test #'equal)))
        (defun ,name (,@args)
          ,declarations
@@ -15,11 +21,14 @@
                          (setf (gethash (list ,@args) ,h) (lambda () ,result))
                          ,result)))))))))
 
-(defmacro deflazy/memo (name args &body body)
-  `(deflazy/memo-declare ,name ,args () ,@body))
-
-(defmacro defmemo-declare (name args declarations &body body)
-  (let ((h (gensym "HASH")))
+(defmacro defmemo (name args &body body)
+  (let ((h (gensym "HASH"))
+        (declarations ()))
+    (when (and (listp body)
+               (listp (car body))
+               (equal 'declare (caar body)))
+      (setf declarations (car body)
+            body (cdr body)))
     `(let ((,h (make-hash-table :test #'equal)))
        (defun ,name (,@args)
          ,declarations
@@ -31,21 +40,22 @@
                (setf (gethash (list ,@args) ,h)
                      (progn ,@body))))))))
 
-(defmacro defmemo (name args &body body)
-  `(defmemo-declare ,name ,args () ,@body))
-
 (deflazy/memo fib-lazymemo (n)
+  (declare (fixnum n))
+;  (format t "Evaluating: (fib-lazymemo ~A) ...~%" n)
   (if (<= n 2)
       1
       (+ (funcall (fib-lazymemo (1- n))) (funcall (fib-lazymemo (- n 2))))))
 
 (defun fib-slow (n)
-  (declare (fixnum n))
+;  (format t "Evaluating: (fib-slow ~A) ...~%" n)
   (if (<= n 2)
       1
       (+ (fib-slow (1- n)) (fib-slow (- n 2)))))
 
 (defmemo fib-memo (n)
+  (declare (fixnum n))
+;  (format t "Evaluating: (fib-memo ~A) ...~%" n)
   (if (<= n 2)
       1
       (+ (fib-memo (1- n)) (fib-memo (- n 2)))))
