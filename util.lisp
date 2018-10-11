a468cfa086d557dc1c97bb99a0053b312f6604b3
XZentus <z_xmn@mail.ru>
2018-10-11 10:53:32 +0300

    Update util.lisp

--- util.lisp
+++ util.lisp
@@ -13,3 +13,31 @@
                        (let ((,result (progn ,@body)))
                          (setf (gethash (list ,@args) ,h) (lambda () ,result))
                          ,result)))))))))
+
+(defmacro defmemo (name args &body body)
+  (let ((h (gensym "HASH")))
+    `(let ((,h (make-hash-table :test #'equal)))
+       (defun ,name (,@args)
+         (multiple-value-bind
+               (val foundp)
+             (gethash (list ,@args) ,h)
+           (if foundp
+               val
+               (setf (gethash (list ,@args) ,h)
+                     (progn ,@body))))))))
+
+
+(deflazy/memo fib-lazymemo (n)
+  (if (<= n 2)
+      1
+      (+ (funcall (fib (1- n))) (funcall (fib (- n 2))))))
+
+(defun fib-slow (n)
+  (if (<= n 2)
+      1
+      (+ (fib-slow (1- n)) (fib-slow (- n 2)))))
+
+(defmemo fib-memo (n)
+  (if (<= n 2)
+      1
+      (+ (fib-memo (1- n)) (fib-memo (- n 2)))))
