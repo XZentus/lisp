0383bfcdf40a2000ee590e9f83eaaed8992cf99b
XZentus <z_xmn@mail.ru>
2018-10-25 11:26:51 +0300

    Update e19.lisp

--- e19.lisp
+++ e19.lisp
@@ -1,5 +1,5 @@
 (defpackage :e19
-  (:use :cl))
+  (:use :cl :iterate))
 
 (in-package :e19)
 
@@ -16,3 +16,16 @@
                  29
                  28))
     (t (aref #(0 31 0 31 30 31 30 31 31 30 31 30 31) m))))
+
+(defun next-month-starting-day (d m)
+  (rem (+ d m) 7))
+
+;; (let ((d 1)
+;;       (mon1 0))
+;;   (iter (for y from 1901 to 2000)
+;;     (iter (for m from 1 to 12)
+;;       (when (= d 6)
+;;         (format t "~A ~A ~A~%" y m d)
+;;         (incf mon1))
+;;       (setf d (next-month-starting-day d (days-in-month m y)))))
+;;   (values d mon1))
